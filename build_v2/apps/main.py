9b58e8498721d321ee64d6373068dcbc4fbe42a8
shinilseop <shinilseop12@gmail.com>
2022-10-17 14:30:53 +0900

    chore: not used bentoml runner

--- build_v2/apps/main.py
+++ build_v2/apps/main.py
@@ -8,21 +8,11 @@ output_type = utils.get_data_type(os.environ['OUTPUT_TYPE'])
 model_name = os.environ['MODEL_NAME']
 library = os.environ['LIBRARY']
 
-<<<<<<< HEAD
 model = prepared.load_model()
 svc = bentoml.Service(model_name)
-=======
-runner = utils.get_runner(library, model_name)
-svc = bentoml.Service(model_name, runners=[runner])
-
->>>>>>> b48f64a (feat: add support multiple framework (include detectron2))
 
 @svc.api(input=input_type, output=output_type)
 def predict(input):
     model_input = prepared.pre_processing(input)
-<<<<<<< HEAD
     model_output = prepared.predict(model, model_input)
-=======
-    model_output = prepared.predict(runner, model_input)
->>>>>>> b48f64a (feat: add support multiple framework (include detectron2))
     return prepared.post_processing(model_output)
--- build_v2/build.sh
+++ build_v2/build.sh
@@ -10,10 +10,7 @@ LIBRARY=""
 USERNAME=""
 PASSWORD=""
 REGISTRY=""
-<<<<<<< HEAD
 TYPE=""
-=======
->>>>>>> b48f64a (feat: add support multiple framework (include detectron2))
 
 # 0. parsed options
 while (("$#")); do
@@ -99,7 +96,6 @@ while (("$#")); do
                 exit 1
             fi
             ;;
-<<<<<<< HEAD
         --type) # set image registry name
             if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
                 TYPE=$2
@@ -113,8 +109,6 @@ while (("$#")); do
                 exit 1
             fi
             ;;
-=======
->>>>>>> b48f64a (feat: add support multiple framework (include detectron2))
         -h|--help) # help
             echo "Usage: $0 -i <input_type>" >&2
             echo "      -f | --file       (set custom file url)" >&2
@@ -125,10 +119,7 @@ while (("$#")); do
             echo "      -l | --library    (set machine learning framework)" >&2
             echo "      -u | --user       (set image registry username)" >&2
             echo "      -p | --password   (set image registry password)" >&2
-<<<<<<< HEAD
             echo "      --type   (set instance type cpu or gpu)" >&2
-=======
->>>>>>> b48f64a (feat: add support multiple framework (include detectron2))
             exit 0
             ;;
         -*|--*) # unsupported flags
@@ -146,21 +137,12 @@ done
 
 IMAGE_TAG=""
 if [ -z $REGISTRY ]; then
-<<<<<<< HEAD
     IMAGE_TAG="$USERNAME/$MODEL_NAME:$TAG"
 else
     IMAGE_TAG="$REGISTRY/$USERNAME/$MODEL_NAME:$TAG"
 fi
 
 echo "====================================parsed options====================================" >&1
-=======
-    IMAGE_TAG="${REGISTRY}/${USERNAME}/${MODEL_NAME}:${TAG}"
-else
-    IMAGE_TAG="${USERNAME}/${MODEL_NAME}:${TAG}"
-fi
-
-echo "===============================parsed options===============================" >&1
->>>>>>> b48f64a (feat: add support multiple framework (include detectron2))
 echo "FILE_URL      : ${FILE_URL}" >&1
 echo "INPUT_TYPE    : $INPUT_TYPE" >&1
 echo "OUTPUT_TYPE   : ${OUTPUT_TYPE}" >&1
@@ -170,10 +152,7 @@ echo "LIBRARY       : ${LIBRARY}" >&1
 echo "USERNAME      : ${USERNAME}" >&1
 echo "REGISTRY      : ${REGISTRY}" >&1
 echo "IMAGE_TAG     : ${IMAGE_TAG}" >&1
-<<<<<<< HEAD
 echo "INSTANCE TYPE : ${TYPE}" >&1
-=======
->>>>>>> b48f64a (feat: add support multiple framework (include detectron2))
 echo ""
 echo ""
 
@@ -233,7 +212,6 @@ case "$FILE_TYPE" in
         ;;
 esac
 
-<<<<<<< HEAD
 echo "==============================Python Dependencies Install===============================" >&1
 if [ -e "requirements.txt" ]; then
     # cat requirements.txt | xargs -n 1 pip3 install
@@ -276,25 +254,13 @@ exit 0
 
 # 3. bentoml model save
 # echo "=================================Model SAVE to BentoML=================================" >&1
-=======
-echo "=========================Python Dependencies Install==========================" >&1
-if [ -e "requirements.txt" ]; then
-    pip3 install -r requirements.txt
-    cat requirements.txt >> bento_requirements.txt
-fi
-
-# 3. bentoml model save
-echo "============================Model SAVE to BentoML============================" >&1
->>>>>>> b48f64a (feat: add support multiple framework (include detectron2))
 export INPUT_TYPE
 export OUTPUT_TYPE
 export MODEL_NAME
 export LIBRARY
-<<<<<<< HEAD
 # python3.8 save.py
 
 # 4. bentoml build
-<<<<<<< HEAD
 echo "==================================BENTOML BUILD SCRIPT==================================" >&1
 if [ "$TYPE" == "cpu" ]; then
     sed "s/{input_type}/$INPUT_TYPE/g;s/{output_type}/$OUTPUT_TYPE/g;s/{model_name}/$MODEL_NAME/g;s/{library}/$LIBRARY/g" bentofile_cpu.template > bentofile.yaml
@@ -302,10 +268,6 @@ else
     sed "s/{input_type}/$INPUT_TYPE/g;s/{output_type}/$OUTPUT_TYPE/g;s/{model_name}/$MODEL_NAME/g;s/{library}/$LIBRARY/g" bentofile_gpu.template > bentofile.yaml
 fi
 echo "-------------bentofile.yaml--------------" >&1
-=======
-echo "=============================BENTOML BUILD SCRIPT=============================" >&1
-sed "s/{input_type}/$INPUT_TYPE/g;s/{output_type}/$OUTPUT_TYPE/g;s/{model_name}/$MODEL_NAME/g;s/{library}/$LIBRARY/g" bentofile.template > bentofile.yaml
->>>>>>> 67f1062 (feat: add library settings)
 cat bentofile.yaml >&1
 echo "-----------Dockerfile.template-----------" >&1
 cat Dockerfile.template >&1
@@ -317,28 +279,11 @@ bentoml build
 
 # 5. bentoml containerizing
 echo "=================================CONTAINERIZATION START=================================" >&1
-=======
-python3.8 save.py
-
-# 4. bentoml build
-echo "=============================BENTOML BUILD SCRIPT=============================" >&1
-sed "s/{input_type}/$INPUT_TYPE/g;s/{output_type}/$OUTPUT_TYPE/g;s/{model_name}/$MODEL_NAME/g" bentofile.template > bentofile.yaml
-cat bentofile.yaml >&1
-echo "=============================BENTOML BUILD START==============================" >&1
-bentoml build
-
-# 5. bentoml containerizing
-echo "============================CONTAINERIZATION START============================" >&1
->>>>>>> b48f64a (feat: add support multiple framework (include detectron2))
 echo "bentoml containerize $MODEL_NAME:latest -t ${IMAGE_TAG} --network host --verbose --platform=linux/amd64" >&1
 bentoml containerize $MODEL_NAME:latest -t ${IMAGE_TAG} --network host --verbose --platform=linux/amd64
 
 # 6. docker push
-<<<<<<< HEAD
 echo "=================================CONTAINERIZATION PUSH==================================" >&1
-=======
-echo "============================CONTAINERIZATION PUSH=============================" >&1
->>>>>>> b48f64a (feat: add support multiple framework (include detectron2))
 echo "$PASSWORD" | docker login --username $USERNAME --password-stdin
 echo "docker push ${IMAGE_TAG}"
 docker push ${IMAGE_TAG}
